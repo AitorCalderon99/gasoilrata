867a4b480b436558a945a4018211553a190d2386
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2022-01-10 13:23:19 +0100

    Empezado el general
    · Subido el logo, falta eliminarle el fondo blanco
    · Creado el general.blade
    · Cambiada la ruta de la web.php para que habra el general

---
+++ retogasoilrata/resources/views/layouts/general.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Reto</title>
+    <!--Favicon-->
+    <link rel="shortcut icon" href="{{ asset('Logo.ico') }}">
+</head>
+<body>
+    <h1>hola</h1>
+</body>
+</html>
