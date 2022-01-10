c2d9b38c9ae3db83f66b829a36bbb294d8f509c6
AitorCalderon99 <90387604+AitorCalderon99@users.noreply.github.com>
2022-01-10 10:54:56 +0100

    se añaden migraciones

---
+++ retogasoilrata/database/migrations/2022_01_10_090027_create_usuarios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('id_usuario');
+            $table->string('nombre', 24);
+            $table->string('email', 50)->unique();
+            //$table->timestamp('email_verified_at')->nullable();
+            $table->string('ubicacion', 15);
+            $table->string('password', 20);
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
