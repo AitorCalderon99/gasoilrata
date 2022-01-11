d957b8cb095da359480a8eeda3c39cf3038f33b3
Seilfox <73230206+Seilfox@users.noreply.github.com>
2022-01-11 09:37:45 +0100

    Update consumosController.php

--- retogasoilrata/app/Http/Controllers/consumosController.php
+++ retogasoilrata/app/Http/Controllers/consumosController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class consumosController extends Controller
+class ConsumosController extends Controller
 {
     /**
      * Display a listing of the resource.
