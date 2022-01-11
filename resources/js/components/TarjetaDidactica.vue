9d07375338cfb0224f3d8ecdff7bae620e757d93
Yeray <yerayliloaiza@gmail.com>
2022-01-11 14:21:44 +0100

    creado componente tarjeta didactica

---
+++ resources/js/components/TarjetaDidactica.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="tarjeta container">
+        <div class="adelante">
+            <h3 class="pregunta">
+
+            </h3>
+        </div>
+
+        <div class="atras">
+            <p class="respuesta">
+                
+            </p>
+        </div>
+    </div>
+</template>
