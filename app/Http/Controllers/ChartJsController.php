f3b9ae3c3ce9d8c8e23e1678737a00fb01c14fc8
Yeray <yerayliloaiza@gmail.com>
2022-01-12 12:44:05 +0100

    controlador para chartjs

---
+++ app/Http/Controllers/ChartJsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use DB;
+use Carbon\Carbon;
+
+class ChartJsController extends Controller
+{
+    public function index() {
+        
+    }
+}
