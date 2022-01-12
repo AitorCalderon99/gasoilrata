7ae881db5b0c0b61b5dbeda594eaf1e914278720
Yeray <yerayliloaiza@gmail.com>
2022-01-12 08:49:45 +0100

    componente tarjetas y json con 2 preguntas

--- resources/js/components/TarjetaDidactica.vue
+++ resources/js/components/TarjetaDidactica.vue
@@ -1,15 +1,37 @@
 <template>
-    <div class="tarjeta container">
-        <div class="adelante">
-            <h3 class="pregunta">
-
-            </h3>
-        </div>
-
-        <div class="atras">
-            <p class="respuesta">
-                
-            </p>
+    <div class="tarjetas">
+        <div class="tarjeta card" v-for="cuestion in cuestiones" :key="cuestion">
+            <div class="adelante">
+                <h3 class="pregunta">
+                    {{ cuestion.pregunta }}
+                </h3>
+            </div>
+            <div class="atras">
+                <p class="respuesta">
+                    {{ cuestion.respuesta }}
+                </p>
+            </div>
         </div>
     </div>
 </template>
+
+<script>
+    import tarjetas from "../tarjetas.json";
+
+    var cuestiones = [];
+
+    for (var i = 0 ; i < 2 ; i++) {
+        // numero aleatorio entre 0 y longitud de array tarjetas
+        var numRandom = Math.floor(Math.random() * (tarjetas.length +1));
+
+        cuestiones.push(tarjetas[numRandom]);
+    }
+
+    module.exports = {
+        data : function() {
+            return {
+                cuestiones : cuestiones
+            }
+        }
+    }
+</script>
