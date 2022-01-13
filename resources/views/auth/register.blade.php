423a61a4f4a953448651acf0854b771fd2e38339
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2022-01-13 14:20:27 +0100

    Estilos
    · Estilo de la pagina de recuperar cuenta hecho
    · Estilo de registrarse empezado

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,87 @@
 <x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+    <!-- Validation Errors -->
+    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+    <form method="POST" action="{{ route('register') }}" class="container">
+        @csrf
+
+        <!-- Name -->
+        <div>
+            <x-label for="name" :value="__('Nombre')" class="form-label fw-bold text-light"/>
+
+            <x-input id="name" class="form-control rounded-pill" type="text" name="name" :value="old('name')" required autofocus />
+        </div>
+
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-label for="email" :value="__('Correo')" class="form-label fw-bold text-light"/>
+
+            <x-input id="email" class="form-control rounded-pill" type="email" name="email" :value="old('email')" required />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-label for="password" :value="__('Contraseña')" class="form-label fw-bold text-light"/>
+
+            <x-input id="password" class="form-control rounded-pill"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-label for="password_confirmation" :value="__('Confirmar contraseña')" class="form-label fw-bold text-light"/>
+
+            <x-input id="password_confirmation" class="form-control rounded-pill"
+                            type="password"
+                            name="password_confirmation" required />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                {{ __('¿Ya tienes una cuenta?') }}
             </a>
-        </x-slot>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
+            <button class="ml-4">
+                {{ __('Registrarse') }}
+            </button>
+        </div>
+    </form>
 </x-guest-layout>
+
+<style>
+    html * {
+        font-family: "Roboto";
+    }   
+
+    a{
+        text-decoration: none;
+        transition: 0.5s;
+    }
+
+    a:hover{
+        font-weight: bold;
+    }
+
+    button{
+        width: 10em;
+        height: 3em;
+        border: none;
+        font-weight: 200;
+    }
+
+    #contrasenia > input{
+        padding-top: 6px;
+        padding-bottom: 2px;
+    }
+
+    input{
+        opacity: 56%;
+    }
+
+    body{
+        background-color: #C5A880;
+    }
+</style>
+
