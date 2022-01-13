3785c8180b9bf8895b0237186875e92a4f41fd02
Seilfox <73230206+Seilfox@users.noreply.github.com>
2022-01-13 12:57:13 +0100

    sdfsd

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.app')
 @section('content')
 
-    <h1>prueba contacto</h1>
+    <div id="main">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2905.701793571246!2d-2.9045955845150373!3d43.25767127913678!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4e4fac87437727%3A0x364f27e82def0130!2sCIFP%20Txurdinaga%20LHII!5e0!3m2!1ses!2ses!4v1642074299000!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+        <div>
+            <p>idazkaria@fpTXurdinaga.com</p>
+        </div>
+        <div>
+            <p>C/ Doctor Ornilla 2, 48004 BILBAO</p>
+        </div>
+        <div>
+            <p>Horario:</p>
+            <div>
+                <p>Lunes: 9:00 - 15:00</p>
+                <p>Miércoles: 9:00 - 18:30</p>
+                <p>Martes, jueves y viernes: 9:00 - 14:00</p>
+            </div>
+        </div>
+        <div>
+            <p>944 125 712</p>
+        </div>
+    </div>
     
 @endsection
