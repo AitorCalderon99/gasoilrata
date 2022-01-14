3bb75a2f904860b719538c75037df127a7ae313d
Yeray <yerayliloaiza@gmail.com>
2022-01-14 09:48:43 +0100

    etiqueta stack js y css | movidas etiquetas script y style al head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>@yield('title')</title>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
+
+    @stack('script')
+    @stack('style')
 </head>
 <body>
     <div id="app">
@@ -17,7 +24,5 @@
         <app></app>
     </div>
 
-    <script src="{{ mix('js/app.js') }}"></script>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
 </body>
 </html>
