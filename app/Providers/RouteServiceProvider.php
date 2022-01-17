c5f50349bd10ba10236641763da53fd883fb065b
Seilfox <73230206+Seilfox@users.noreply.github.com>
2022-01-17 11:36:56 +0100

    sad

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '../../resources/views/index.blade.php';
 
     /**
      * The controller namespace for the application.
