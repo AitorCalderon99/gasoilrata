b497b58469ded7ba4efa4f09d753c12278564f29
Yeray <yerayliloaiza@gmail.com>
2022-01-17 12:07:35 +0100

    Vue 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,43 +4,20 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
 global.$ = global.jQuery = require('jquery');
 
-import { createApp } from 'vue'
-import App from './components/Header.vue'
-import App2 from './components/Footer.vue'
-import TarjetaDidactica from './components/TarjetaDidactica.vue'
-//import router from './router'
-//import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
-const app =  createApp(App).mount('#app');
-const app2 = createApp(App2).mount('#app2');
-const tarjeta = createApp(TarjetaDidactica).mount('#slider-tarjetas');
+import { createApp } from 'vue';
+const app = createApp({});
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+app.component('Header', Header);
+app.component('Footer', Footer);
+app.use(BootstrapVue3);
+app.mount('#app');
 
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-/* Vue.component('example-component', require('./components/ExampleComponent.vue').default); */
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-/*
-Vue.component('headeras', require('./components/Header.vue').default);
-Vue.component('pie', require('./components/Footer.vue').default);
-Vue.component('TarjetaDidactica', require('./components/TarjetaDidactica.vue').default);
-*/
