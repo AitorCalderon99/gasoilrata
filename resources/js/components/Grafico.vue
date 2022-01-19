2b40d360e8fec5b583efa4119f77d48e5f022da4
Yeray <yerayliloaiza@gmail.com>
2022-01-19 09:25:40 +0100

    creado gráfico de barras

---
+++ resources/js/components/Grafico.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container-fluid my-2">
+    <vue3-chart-js
+        :id="barChart.id"
+        :type="barChart.type"
+        :data="barChart.data"
+        @before-render="beforeRenderLogic"
+    ></vue3-chart-js>
+  </div>
+</template>
+
+<script>
+import Vue3ChartJs from '@j-t-mcc/vue3-chartjs'
+
+export default {
+  name: 'App',
+  components: {
+    Vue3ChartJs,
+  },
+  setup () {
+    const barChart = {
+      id: 'bar',
+      type: 'bar',
+      data: {
+          //labels es el eje x
+        labels: ['Gasóleo A', 'Gasolina95', 'Gasolina 98'],
+        datasets: [
+          {
+            backgroundColor: [
+              '#41B883',
+              '#E46651',
+              '#00D8FF'
+            ],
+            data: [1.400, 1.521, 1.677]
+          }
+        ]
+      }
+    }
+  
+    const beforeRenderLogic = (event) => {
+      //...
+      //if(a === b) {
+      //  event.preventDefault()
+      //}   
+    }    
+
+    return {
+      barChart,
+      beforeRenderLogic
+    }
+  },
+}
+</script>
