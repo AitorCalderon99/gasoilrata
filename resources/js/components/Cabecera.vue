260b8993237d7eff646b56a78ffc225166e182ce
Yeray <yerayliloaiza@gmail.com>
2022-01-20 14:11:36 +0100

    retocado header

--- resources/js/components/Cabecera.vue
+++ resources/js/components/Cabecera.vue
@@ -1,17 +1,24 @@
 <template>
   <nav class="nav nav-pills nav-justified">
-    <button
-      type="button"
-      class="navbar-toggler"
-      data-bs-toggle="collapse"
-      data-bs-target="#hamburguesa"
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-</svg>
-    </button>
-    <p class="nav-item my-auto fs-1">GASOIL</p>
-    <a href="index.php" class="nav-item nav-link"><img id="Logo" src="Logo.png" /></a>
+
+    <div class="hamburguer-contenedor contenedor">
+      <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#hamburguesa">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+      </button>
+    </div>
+
+    <div class="titulo-contenedor contenedor">
+      <p class="nav-item">GASOILRATA</p>
+    </div>
+
+    <div class="logo-contenedor contenedor">
+      <a href="index.php" class="nav-item nav-link">
+        <img id="Logo" src="Logo.png" alt="Logo de GasoilRat"/>
+      </a>
+    </div>
+
     <div id="hamburguesa" class="collapse navbar-collapse">
       <div class="navbar-nav">
         <a href="/" class="nav-item">Inicio</a>
@@ -22,53 +29,58 @@
         <a href="registrarse" class="nav-item">Hacerse miembro</a>
       </div>
     </div>
+
   </nav>
 </template>
 
 
 <style scoped>
 html * {
-  font-family: "Roboto";
-}
-nav{
-  font-style: italic;
-}
-
-a:not(#hamburguesa a) {
-  font-weight: bold;
-  color: black;
-}
-
-.nav-justified .nav-item,
-.nav-justified > .nav-link {
-  text-align: end;
-}
-
-#hamburguesa a {
-  color: white;
-  text-decoration: none;
-  transition: 0.5s;
-  width: fit-content;
-  margin: auto;
-}
-
-#hamburguesa a:hover {
-  color: black;
-}
-
-#Logo {
-  width: 30px;
-  height: 30px;
-  margin-right: 0%;
+  font-family: "Hemi Head";
 }
 
 nav {
   background-color: #c5a880;
 }
 
-.container {
-  width: 100%;
-  margin: 0%;
-  padding: 0%;
+.nav-justified .nav-item,
+.nav-justified > .nav-link {
+  text-align: center;
+}
+
+svg {
+  width: 2rem;
+  max-height: auto;
+}
+
+#hamburguesa a {
+  color: black;
+  text-decoration: none;
+  transition: 0.2s;
+  width: fit-content;
+  margin: auto;
+  font-size: x-large;
+}
+
+#hamburguesa a:hover {
+  text-decoration: underline;
+}
+
+#hamburguesa a:active {
+  color: white;
+}
+
+#Logo {
+  width: 2.5rem;
+  max-height: auto;
+  text-align: end;
+}
+
+.contenedor {
+  flex: 1 1 auto;
+}
+
+.titulo-contenedor p {
+  font-size: xx-large;
 }
 </style>
