9d74b22785cdd8d91552561f2b1ef0ffc39fbced
AitorCalderon99 <90387604+AitorCalderon99@users.noreply.github.com>
2022-01-24 08:06:23 +0100

    cambio de nombre a user.name en factory model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ class UserFactory extends Factory
         $faker = Faker\Factory::create('es_ES');
 
         return [
-            'nombre' => $this->faker->name(),
+            'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'ubicacion' => $faker->state(),
