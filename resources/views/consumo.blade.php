9236fb239e8e5025140391fdaa676a0f3971bf92
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2022-01-28 13:37:54 +0100

    textos

--- resources/views/consumo.blade.php
+++ resources/views/consumo.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 
     <main>
-        <h3 class="text-center">¡WOW, qué cantidad de dinero que has ahorrado utilizando la calculadora de consumo!</h3>
+        <h3 class="my-4 text-center">¡WOW, qué cantidad de dinero que has ahorrado utilizando la calculadora de consumo!</h3>
 
         <Vehiculo_seleccion></Vehiculo_seleccion>
 
--- resources/views/repostajes.blade.php
+++ resources/views/repostajes.blade.php
@@ -8,13 +8,16 @@
 
 @section('content')
 <main>
-
     <div class="crear-repostaje">
-        <h1>¿Quieres crear un nuevo repostaje?</h1>
-        <a class="btn btn-primary" href="crearRepostajes">Crear</a>
+        <h1 class="text-center mt-4 mb-3">¿Quieres crear un nuevo repostaje?</h1>
+        <div class="d-flex align-items-center justify-content-center">
+            <a class="btn btn-primary" href="crearRepostajes">
+                Crear
+            </a>
+        </div>
     </div>
 
-    <h1>¡Revisa el historial de repostajes para ahorrarte un dinerito!</h1>
+    <h1 class="text-center mt-5 mb-3">¡Revisa el historial de repostajes para ahorrarte un dinerito!</h1>
 
     <Vehiculo_seleccion></Vehiculo_seleccion>
 
