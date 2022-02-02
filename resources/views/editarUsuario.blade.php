2272c2df34876ee49a43a67d9e24f90e743e1f66
Yeray <yerayliloaiza@gmail.com>
2022-02-02 11:38:23 +0100

    la guerra de las galias TERMINADO

--- resources/views/editarUsuario.blade.php
+++ resources/views/editarUsuario.blade.php
@@ -1,30 +1,100 @@
 @extends('layouts.app')
 @section('content')
 
-<div id="main">lkhsdfilsdhbfkjsdh
+<div id="main">
     <h1>JULIO CÉSAR, HISTORIADOR DE SÍ MISMO</h1>
     <p>Cayo Julio César, que es uno de los tres más grandes capitanes de la Historia con Alejandro
-Magno y con Napoleón, es también uno de los tres más considerables historiadores latinos, con
-Cayo Crispo Salustio y con Tito Livio, formando el ejemplar triunvirato del período clásico por
-excelencia, período verdaderamente «áureo» de las letras latinas. Y Julio César es todo esto, tiene
-tal significación, precisamente como historiador de sí mismo, narrador de sus propias hazañas
-guerreras y de su política. </p>
+        Magno y con Napoleón, es también uno de los tres más considerables historiadores latinos, con
+        Cayo Crispo Salustio y con Tito Livio, formando el ejemplar triunvirato del período clásico por
+        excelencia, período verdaderamente «áureo» de las letras latinas. Y Julio César es todo esto, tiene
+        tal significación, precisamente como historiador de sí mismo, narrador de sus propias hazañas
+        guerreras y de su política. </p>
 
-<p>Había en él, además de un excepcional militar y un no menos extraordinario estadista y
-gobernante, un admirable literato, más plural o polifacético de lo que, por lo común, suele saberse;
-un literato al que, por haberse perdido varias de sus obras ajenas al género histórico, no podemos
-juzgar en su integridad y de modo directo, pero sin duda no muy inferior al historiador en el cultivo
-de otras manifestaciones literarias, distintas a lo histórico; un literato, en fin, autor del poema El
-viaje, de la tragedia Edipo y de otras creaciones poéticas, del Anti-Catón, de una astronomía De
-astris y de un tratado acerca de los augures y los auspicios</p>
+    <p>Había en él, además de un excepcional militar y un no menos extraordinario estadista y
+        gobernante, un admirable literato, más plural o polifacético de lo que, por lo común, suele saberse;
+        un literato al que, por haberse perdido varias de sus obras ajenas al género histórico, no podemos
+        juzgar en su integridad y de modo directo, pero sin duda no muy inferior al historiador en el cultivo
+        de otras manifestaciones literarias, distintas a lo histórico; un literato, en fin, autor del poema El
+        viaje, de la tragedia Edipo y de otras creaciones poéticas, del Anti-Catón, de una astronomía De
+        astris y de un tratado acerca de los augures y los auspicios</p>
 
-<p>Con todo, le bastan sus obras de carácter histórico, sobre la historia que él mismo vivió e
-hizo, protagonizándola, para que le juzguemos conforme se dice al principio de estas líneas; obras
-evidentemente originales, redactadas sin asistencia de persona alguna, en las que, lejos de imitar, se
-haría digno de imitación, afirmando notables cualidades y condiciones de historiador, de maestro de
-la historia narrativa. Sobrio y preciso, claro y metódico, brillante y colorista sin alardes, de acuerdo
-con la austeridad y la severidad propias del género en sus más dignas concepciones..., así es Julio
-César, historiador de sí mismo. 
-</p>
+    <p>Con todo, le bastan sus obras de carácter histórico, sobre la historia que él mismo vivió e
+        hizo, protagonizándola, para que le juzguemos conforme se dice al principio de estas líneas; obras
+        evidentemente originales, redactadas sin asistencia de persona alguna, en las que, lejos de imitar, se
+        haría digno de imitación, afirmando notables cualidades y condiciones de historiador, de maestro de
+        la historia narrativa. Sobrio y preciso, claro y metódico, brillante y colorista sin alardes, de acuerdo
+        con la austeridad y la severidad propias del género en sus más dignas concepciones..., así es Julio
+        César, historiador de sí mismo.
+    </p>
+
+    <p>Cayo Julio César, nacido en Roma el 12 de julio del año -100, perteneció a una de las
+        familias más distinguidas de Roma, habiendo desde sus primeros años manifestado una inteligencia
+        y una elevación de ánimo en las que se preanunciaba su futura grandeza. A los trece años, fue
+        nombrado sacerdote de Júpiter, y a los dieciocho, contrajo matrimonio con Cornelia, hija de Cinna,
+        dando con esta ocasión una prueba de la firmeza de su carácter, al oponerse a la orden de Sila,
+        entonces dueño absoluto de Roma, de que repudiase a su esposa. Más adelante se trasladó a Asia,
+        incorporado al ejército, destacándose allí, por su heroico comportamiento, en el sitio de Mitilene</p>
+
+    <p>Regresó a Roma a la muerte de Sila y de allí pasó, poco después, a Rodas, a fin de
+        perfeccionarse en la elocuencia, volviendo a Roma en el año -74. Entonces dio comienzo a su vida
+        política, poniéndose al frente del partido popular, contra el Senado y los patricios, sostenidos a la
+        sazón por Pompeyo. No tardó, por su habilidad y su elocuencia, en verse convertido en el ídolo de
+        las multitudes, alcanzando, con su favor, los más altos cargos del Estado. En el -67, fue cuestor;
+        edil, en el -65; pontífice máximo, en el -63, y nuevamente cuestor, en el -62. Fue acusado de haber
+        tomado parte en la conspiración de Catilina, pero supo defenderse con tal habilidad que salió del
+        tribunal aclamado por el pueblo y paseado en triunfo por las calles de Roma</p>
+
+    <p>A crecido con ello su poder, obtuvo en el año -61 el gobierno de la España Ulterior, donde
+        mostró una vez más sus grandes dotes para el mando y para la política. Regresó de España
+        vencedor, reclamado en Roma por los acontecimientos, y por la inestabilidad política, provocada
+        por Pompeyo y los enemigos de este general, que hacía presagiar graves males para la República.
+        Llegado a Roma, se atrajo César de nuevo el favor del pueblo, por haber renunciado al triunfo que
+        se le debía. Se afanó entonces para conseguir un acuerdo con Pompeyo y con Craso, lográndolo al
+        fin, y quedando de este modo constituido el Primer Triunvirato.</p>
+
+    <p>Al año siguiente, César se hacía nombrar cónsul y antes de expirar el término de su
+        consulado conseguía su nombramiento de gobernador de la Galia, donde una invasión de los
+        germanos le ofrecía entonces la magnífica ocasión que esperaba para aumentar aún su gloria y su
+        poder</p>
+
+    <p>La historia de sus luchas en la Galia constituye el tema de La guerra de las Galias, que
+        ofrecemos hoy a nuestros lectores. Siete de los ocho libros que componen esta obra se consideran como escritos
+        por el propio César. Por la claridad y maravillosa sencillez de su estilo, se coloca su
+        autor entre los primeros escritores de su tiempo; el octavo lo escribió Aulo Hircio, uno de sus
+        generales, al parecer sobre notas dejadas por el propio César, y en el que se esfuerza por imitar a su
+        jefe. Sigue luego, en el volumen próximo, La Guerra Civil, escrita también por César y en la que se
+        narran los acontecimientos de aquel período agitado de la historia de Roma, con el triunfo final de
+        César</p>
+
+    <p>Tras ésta, también en el volumen próximo, ofreceremos al público La guerra de Alejandría,
+        que se atribuye así mismo a Aulo Hircio, y los comentarios de la Guerra de África y Guerra de
+        España, que completan la serie de estos libros. Se ignora quiénes son los autores de estos últimos, y
+        su mérito literario es muy inferior a los del propio Hircio; pero, siguiendo con esto el criterio
+        adoptado en la mayoría de las ediciones extranjeras, hemos querido ofrecer al público el relato
+        completo de las campañas en que tomó parte César, hasta el exterminio de los últimos partidarios de
+        Pompeyo, con su hijo, que refugiados en África y España, le ofrecían aún resistencia.</p>
+
+    <p>Para el período de la vida de César que va desde aquí hasta su muerte, remitimos al lector,
+        ya sea a la Vida de César, que figura en Los doce Césares de Suetonio (volumen 7 de esta
+        colección), ya a la del propio general, en las Vidas paralelas de Plutarco (que serán publicadas en
+        nuestra colección en números posteriores).</p>
+
+    <p>En cuanto a la traducción, hemos adoptado la que el señor Goya Muniáin hizo de La guerra
+        de las Galias, y la de don Manuel Balbuena, para el resto de los libros, por ser consideradas ambas
+        como las mejores que existen en castellano. No obstante, ambos textos han sido revisados y
+        corregidos en algunos detalles, de acuerdo con las mejores ediciones extranjeras.</p>
+
+    <p>La guerra de las Galias (Bellum Gallicum), principalmente, ha sido reiteradamente vertida
+        en varios idiomas, y desde luego al castellano, en repetidas ocasiones, pero nunca, bien puede
+        afirmarse, como lo hizo don José Goya a finales del siglo último. Y puestos a mencionar las
+        mejores traducciones de esta obra (la cual viene imprimiéndose constantemente, ya en latín, ya
+        vertida a otras lenguas, desde el año 1469), es obligado citarla, prescindiéndose del interés que
+        pudiera suponer el ser la dada aquí, en esta colección, por nosotros.</p>
+
+    <p>Por otra parte, sin anotar otras de menos importancia, citaremos las ediciones críticas, del
+        Bellum Gallicum y del Bellum civile, de A. Kloz (Leipzig, 1921-27); F. Ramorino (Turín, 1902-03);
+        L. A. Constans (París, 1926); P. Favre (París, 1936); R. Schneider (Berlín, 1888); E. Wolffin y A.
+        Miodonsky (Leipzig, 1889)...</p>
+
 </div>
 @section
