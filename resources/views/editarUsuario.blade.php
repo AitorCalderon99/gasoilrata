41153bf233927a9099cf82c7bd0185bcf66d56ab
Yeray <yerayliloaiza@gmail.com>
2022-02-02 10:53:09 +0100

    la guerra de las galias

--- resources/views/editarUsuario.blade.php
+++ resources/views/editarUsuario.blade.php
@@ -2,6 +2,29 @@
 @section('content')
 
 <div id="main">lkhsdfilsdhbfkjsdh
-    <h1>osiadjasoidja</h1>
+    <h1>JULIO CÉSAR, HISTORIADOR DE SÍ MISMO</h1>
+    <p>Cayo Julio César, que es uno de los tres más grandes capitanes de la Historia con Alejandro
+Magno y con Napoleón, es también uno de los tres más considerables historiadores latinos, con
+Cayo Crispo Salustio y con Tito Livio, formando el ejemplar triunvirato del período clásico por
+excelencia, período verdaderamente «áureo» de las letras latinas. Y Julio César es todo esto, tiene
+tal significación, precisamente como historiador de sí mismo, narrador de sus propias hazañas
+guerreras y de su política. </p>
+
+<p>Había en él, además de un excepcional militar y un no menos extraordinario estadista y
+gobernante, un admirable literato, más plural o polifacético de lo que, por lo común, suele saberse;
+un literato al que, por haberse perdido varias de sus obras ajenas al género histórico, no podemos
+juzgar en su integridad y de modo directo, pero sin duda no muy inferior al historiador en el cultivo
+de otras manifestaciones literarias, distintas a lo histórico; un literato, en fin, autor del poema El
+viaje, de la tragedia Edipo y de otras creaciones poéticas, del Anti-Catón, de una astronomía De
+astris y de un tratado acerca de los augures y los auspicios</p>
+
+<p>Con todo, le bastan sus obras de carácter histórico, sobre la historia que él mismo vivió e
+hizo, protagonizándola, para que le juzguemos conforme se dice al principio de estas líneas; obras
+evidentemente originales, redactadas sin asistencia de persona alguna, en las que, lejos de imitar, se
+haría digno de imitación, afirmando notables cualidades y condiciones de historiador, de maestro de
+la historia narrativa. Sobrio y preciso, claro y metódico, brillante y colorista sin alardes, de acuerdo
+con la austeridad y la severidad propias del género en sus más dignas concepciones..., así es Julio
+César, historiador de sí mismo. 
+</p>
 </div>
 @section
