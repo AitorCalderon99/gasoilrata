a2605b741c98911edf164c581a8e85e1d3c2626b
Seilfox <73230206+Seilfox@users.noreply.github.com>
2022-02-03 10:23:07 +0100

    vídeo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,6 +19,10 @@
     <Tabla_precio></Tabla_precio>
 
     <Grafico></Grafico>
+
+    <div class="ratio ratio-16x9">
+        <iframe src="https://www.youtube.com/embed/68wpNyFHNHc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
     
 </main>
 @endsection
